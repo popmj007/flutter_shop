6189d6941c2d54a8adf742d1da193f2386ebeea3
popmj007 <892839806@qq.com>
2019-08-27 17:04:15 +0800

    JSON TO Dart拉取model, 商品类别组件

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_shop/service/service_method.dart';
 import 'package:flutter_shop/model/CategoryBigModel.dart';
 class CategoryPage extends StatefulWidget{
@@ -18,22 +19,87 @@ class _CategoryPageState extends State<CategoryPage>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    _getCategory();
-    return Container(
-      child: Center(
-        child: Text('分类页面'),
+    return Scaffold(
+      appBar: AppBar(title: Text('商品分类',style: TextStyle(color: Colors.white))),
+      body: Row(
+        children: <Widget>[
+          LeftCategoryNav(),
+
+        ],
+
       ),
+    );
+  }
+
+
+}
+
+class LeftCategoryNav extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new _LeftCategoryNavState();
+  }
+
+
+}
+class _LeftCategoryNavState extends State<LeftCategoryNav>{
+
+  List mlist = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _getCategory();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(width: 1,color: Colors.black12)
+        )
+      ),
+      child: ListView.builder(
+        itemCount: mlist.length,
+        itemBuilder: (context,index){
+          return _leftInkWel(index);
+        }
+      )
     );
   }
 
   void _getCategory() async{
     await request('getCategory').then((val){
       var data = json.decode(val.toString());
-      CategoryBigListModel list = CategoryBigListModel.formJson(data['data']);
-      list.data.forEach((item)=>print(item.mallCategoryName));
+      CategoryModel categoryModel = CategoryModel.fromJson(data);
+      categoryModel.data.forEach((item)=>print(item.mallCategoryName));
+      setState(() {
+        mlist = categoryModel.data;
+      });
     });
   }
 
-
+  Widget _leftInkWel(int index){
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10,top: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1,color: Colors.black12)
+          )
+        ),
+        child: Text(mlist[index].mallCategoryName,style: TextStyle(fontSize: ScreenUtil().setSp(28),),),
+      ),
+    );
+  }
 
 }
+
