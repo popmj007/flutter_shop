33b057f5c57e028ea5316f7ccb64052ea7021b7f
popmj007 <892839806@qq.com>
2019-08-28 17:49:30 +0800

    商品二级分类添加全部

--- lib/provide/child_category.dart
+++ lib/provide/child_category.dart
@@ -4,8 +4,14 @@ import 'package:flutter_shop/model/CategoryBigModel.dart';
 class ChildCategory with ChangeNotifier{
   List<BxMallSubDto> childCategoryList = [];
 
-  getChildCategory(List list){
-    childCategoryList = list;
+  getChildCategory(List<BxMallSubDto> list){
+    BxMallSubDto all = BxMallSubDto();
+    all.mallCategoryId = "";
+    all.mallSubId = "";
+    all.mallSubName = "全部";
+    all.comments = "";
+    childCategoryList = [all];
+    childCategoryList.addAll(list);
     print("yxl=============>"+childCategoryList.length.toString());
     notifyListeners();
   }
