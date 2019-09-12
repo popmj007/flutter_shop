360b49b32f74c784f5dca70eb5a5c6f58f36d8f7
popmj007 <892839806@qq.com>
2019-09-12 11:29:14 +0800

    商品详情页toolbar

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -8,19 +8,71 @@ class DetailsPage extends StatelessWidget{
 
   DetailsPage(this.goodsId);
 
+
+
   @override
   Widget build(BuildContext context) {
 
-    _getBackInfo(context);
+
     // TODO: implement build
-    return Container(
-      child: Text('商品id:${goodsId}'),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: (){
+            print('返回上一页');
+            Navigator.pop(context);
+          },
+        ),
+        title: Text('商品详情页'),
+        centerTitle:true,
+
+      ),
+      body: FutureBuilder(
+          future:_getBackInfo(context),
+          builder: (context,snapshot){
+            if(snapshot.hasData){
+              return Container(
+                child: Column(
+                  children: <Widget>[
+
+                  ],
+                ),
+              );
+            }else{
+              return Text('加载中。。。');
+            }
+          }
+      ),
     );
   }
 
-  void _getBackInfo(BuildContext context)async{
+  Future _getBackInfo(BuildContext context)async{
     await Provide.value<DetailsInfoProvide>(context).getGoodsInfo(goodsId);
     print('加载完成。。。。');
   }
 
+/*  Widget goodsID = Container(
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+      color: Colors.amber,
+      border: Border(
+        bottom: BorderSide(width: 0.5,color: Colors.black26)
+      )
+    ),
+    child: Text('商品id:${goodsId}'),
+  );*/
+
+
+
 }
+
+/*class _DetailsPageState extends State<DetailsPage>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }*/
+
+
+
